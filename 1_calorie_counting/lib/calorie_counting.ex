64ae73a668f2a0a07e73cbf7efc129153a283e27
Kerry Buckley <kerryjbuckley@gmail.com>
2022-12-01 11:49:09 +0000

    Day 1 part 2

--- 1_calorie_counting/lib/calorie_counting.ex
+++ 1_calorie_counting/lib/calorie_counting.ex
@@ -1,6 +1,14 @@
 defmodule CalorieCounting do
-  def highest_total(input) do
-    input |> String.split("\n\n") |> Enum.map(&sum/1) |> Enum.max()
+  def highest_total(input), do: highest_n_total(input, 1)
+  def highest_three_total(input), do: highest_n_total(input, 3)
+
+  defp highest_n_total(input, n) do
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(n)
+    |> Enum.sum()
   end
 
   defp sum(lines) do
--- 1_calorie_counting/test/calorie_counting_test.exs
+++ 1_calorie_counting/test/calorie_counting_test.exs
@@ -1,24 +1,49 @@
 defmodule CalorieCountingTest do
   use ExUnit.Case
 
-  test "returns the total for the elf with the most calories" do
-    input = """
-    1000
-    2000
-    3000
+  describe "CalorieCounting.highest_total/1" do
+    test "returns the total for the elf with the most calories" do
+      input = """
+      1000
+      2000
+      3000
 
-    4000
+      4000
 
-    5000
-    6000
+      5000
+      6000
 
-    7000
-    8000
-    9000
+      7000
+      8000
+      9000
 
-    10000
-    """
+      10000
+      """
 
-    assert CalorieCounting.highest_total(input) == 24_000
+      assert CalorieCounting.highest_total(input) == 24_000
+    end
+  end
+
+  describe "CalorieCounting.highest_three_total/1" do
+    test "returns the total for the three elves with the most calories" do
+      input = """
+      1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+      """
+
+      assert CalorieCounting.highest_three_total(input) == 45_000
+    end
   end
 end
