9c72056591aece02f917fab8a59ab3125894a430
Kerry Buckley <kerryjbuckley@gmail.com>
2022-12-02 20:53:11 +0000

    Day 2 part 2

--- 2_rock_paper_scissors/lib/rock_paper_scissors.ex
+++ 2_rock_paper_scissors/lib/rock_paper_scissors.ex
@@ -12,4 +12,18 @@ defmodule RockPaperScissors do
   defp score_round("C X"), do: 7
   defp score_round("C Y"), do: 2
   defp score_round("C Z"), do: 6
+
+  def score_properly(input) do
+    input |> String.split("\n", trim: true) |> Enum.reduce(0, &(&2 + score_round_properly(&1)))
+  end
+
+  defp score_round_properly("A X"), do: 3
+  defp score_round_properly("A Y"), do: 4
+  defp score_round_properly("A Z"), do: 8
+  defp score_round_properly("B X"), do: 1
+  defp score_round_properly("B Y"), do: 5
+  defp score_round_properly("B Z"), do: 9
+  defp score_round_properly("C X"), do: 2
+  defp score_round_properly("C Y"), do: 6
+  defp score_round_properly("C Z"), do: 7
 end
--- 2_rock_paper_scissors/test/rock_paper_scissors_test.exs
+++ 2_rock_paper_scissors/test/rock_paper_scissors_test.exs
@@ -1,11 +1,23 @@
 defmodule RockPaperScissorsTest do
   use ExUnit.Case
 
-  test "calculates the score" do
-    assert RockPaperScissors.score("""
-           A Y
-           B X
-           C Z
-           """) == 15
+  describe "RockPaperScissors.score/1" do
+    test "calculates the score" do
+      assert RockPaperScissors.score("""
+             A Y
+             B X
+             C Z
+             """) == 15
+    end
+  end
+
+  describe "RockPaperScissors.score_properly/1" do
+    test "calculates the score" do
+      assert RockPaperScissors.score_properly("""
+             A Y
+             B X
+             C Z
+             """) == 12
+    end
   end
 end
