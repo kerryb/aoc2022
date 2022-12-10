df5aee65d651cb41161eebaf80aec696779f0b10
Kerry Buckley <kerryjbuckley@gmail.com>
2022-12-10 16:25:27 +0000

    Day 8 part 2

--- 8_treetop/lib/treetop.ex
+++ 8_treetop/lib/treetop.ex
@@ -2,25 +2,34 @@ defmodule Treetop do
   def number_visible(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&trees_in_row/1)
+    |> Enum.map(&trees_in_row(&1, false))
     |> find_visible()
     |> List.flatten()
     |> Enum.count(&elem(&1, 1))
   end
 
-  defp trees_in_row(row) do
-    row
-    |> String.codepoints()
-    |> Enum.map(&{String.to_integer(&1), false})
+  def max_scenic_score(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&trees_in_row(&1, 1))
+    |> calculate_scenic_scores()
+    |> List.flatten()
+    |> Enum.reduce(0, fn {_, score}, acc -> max(score, acc) end)
   end
 
-  defp find_visible(trees) do
-    trees
-    |> Enum.map(&mark_visible_from_left/1)
-    |> Enum.map(&mark_visible_from_left/1)
-    |> transpose()
-    |> Enum.map(&mark_visible_from_left/1)
-    |> Enum.map(&mark_visible_from_left/1)
+  defp trees_in_row(row, default) do
+    row
+    |> String.codepoints()
+    |> Enum.map(&{String.to_integer(&1), default})
+  end
+
+  defp find_visible(trees), do: four_directions(trees, &mark_visible_from_left/1)
+
+  defp calculate_scenic_scores(trees),
+    do: four_directions(trees, &multiply_each_by_visible_to_right/1)
+
+  defp four_directions(trees, fun) do
+    trees |> Enum.map(fun) |> Enum.map(fun) |> transpose() |> Enum.map(fun) |> Enum.map(fun)
   end
 
   # reverses order, so calling twice will check from left and right
@@ -35,6 +44,18 @@ defmodule Treetop do
   defp mark_visible_from_left([head | tail], checked, max_height) do
     mark_visible_from_left(tail, [head | checked], max_height)
   end
+
+  # reverses order, so calling twice will check from left and right
+  defp multiply_each_by_visible_to_right(unchecked, checked \\ [])
+  defp multiply_each_by_visible_to_right([], checked), do: checked
+
+  defp multiply_each_by_visible_to_right([{height, score} | tail], checked) do
+    multiply_each_by_visible_to_right(tail, [{height, score * visible(height, tail)} | checked])
+  end
+
+  defp visible(_, []), do: 0
+  defp visible(height, [{head, _} | tail]) when height > head, do: 1 + visible(height, tail)
+  defp visible(_, _), do: 1
 
   #  Stolen from https://stackoverflow.com/a/42887944/572562
   defp transpose(rows), do: rows |> List.zip() |> Enum.map(&Tuple.to_list/1)
--- 8_treetop/test/treetop_test.exs
+++ 8_treetop/test/treetop_test.exs
@@ -12,4 +12,16 @@ defmodule TreetopTest do
              """) == 21
     end
   end
+
+  describe "Treetop.max_scenic_score/1" do
+    test "returns the highest product of visible distances in each direction" do
+      assert Treetop.max_scenic_score("""
+             30373
+             25512
+             65332
+             33549
+             35390
+             """) == 8
+    end
+  end
 end
